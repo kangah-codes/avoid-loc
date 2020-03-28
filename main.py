2e7bab7eafddf015f0ad96711be1c3e72b3a5874
Joshua Akangah <akangah89@hmail.com>
2020-03-28 15:20:21 +0000

    fixed bugs

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ def distance(lat1, lon1, lat2, lon2):
 	return 12742 * asin(sqrt(a))
 
 def closest(data, v):
-	[print(i) for i in data]
-	return min(data, key=lambda p: distance(v[0],v[1],p[1],p[2]))
+	return min([distance(v[0],v[1],p[1],p[2])] for p in data)
 
 # tempDataList = a.return_dict()
 # v = [5.621913, -0.238955]
